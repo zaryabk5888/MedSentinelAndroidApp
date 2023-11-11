62648915c83727eb0e404de95f30d7b5c7be4a3f
Zaryabk5888 <Zaryabk5888@gmail.com>
2023-11-11 11:46:43 +0500

    navigation compose dependency updated

--- app/src/main/java/com/example/myapplication/blockchainapp/presentation/users/allusers.kt
+++ app/src/main/java/com/example/myapplication/blockchainapp/presentation/users/allusers.kt
@@ -13,22 +13,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Remove
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -164,6 +159,7 @@ fun listToShow(firestore: FirebaseFirestore,) {
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Retailer")
+
             }
         }
      
